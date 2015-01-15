b43a14176a2060968528afd8b3591d3d2c65f47a
Bones687 <neildalton6@gmail.com>
2015-01-14 21:10:20 -0700

    fixed

--- GetHighScore.php
+++ GetHighScore.php
@@ -3,9 +3,9 @@
 	error_reporting(E_ALL);
 	$mysqli = new mysqli('localhost', 'kraczekn_game', 'testing1234');
 
-	$query = “SELECT MIN(Try) FROM Table”;
+	$query = "SELECT MIN(Try) FROM Table";
 	if(!$result = $mysqli->query($query))
 	{
-		die(“db error fool!”);
+		die("db error fool!");
 	}
 	return $result
