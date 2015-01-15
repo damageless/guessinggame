f4606a24772d0efcb4793f6ebd2c834cd9a7f9ca
John <john.kraczek@gmail.com>
2015-01-15 08:14:43 -0700

    updated javascript
    1. I changed the text of the hint to also include the number that was
    guessed this gives the user an update - otherwise if it is still too
    small nothing displays on the page.
    2. added a try again button that refreshes the page after the user has
    won. (this also prevents the user from submitting higher and higher
    numbers each time the guess button is clicked.
    3. i added a listener for the enter key in the text box. this runs the
    check guess javascript function.

--- javascript.js
+++ javascript.js
@@ -1,34 +1,51 @@
- function sendScore() {
-      $.ajax({
-        type: "POST",
-        url: 'SaveScore.php',
-        data: { turns: count },
-        success: function(data) { console.log(data) }
-      });
-    }
+function sendScore() {
+	$.ajax({
+type: "POST",
+url: 'SaveScore.php',
+data: { turns: count },
+success: function(data) { console.log(data) }
+	});
+}
 
-    function getHighScore() {
-      $.get("GetHighScore.php", function(data) {
-        $("#currentHighScore").html(data);
-      });
-    }
+function getHighScore() {
+	$.get("GetHighScore.php", function(data) {
+		$("#currentHighScore").html(data);
+	});
+}
 
-    var x = Math.floor(Math.random() * 10) + 1;
-    var count = 0;
+var x = Math.floor(Math.random() * 10) + 1;
+var count = 0;
 
-    function checkGuess() {
-      count++;
-      guess = Number(document.getElementById('numberGuess').value);
-      if (guess == x) {
-        document.getElementById('hintText').innerHTML = "You won in " + count + " attempts!";
+function checkGuess() {
+	count++;
+	guess = Number(document.getElementById('numberGuess').value);
+	if (guess == x) {
+		document.getElementById('hintText').innerHTML = "You won in " + count + " attempts!";
 
-        sendScore();
-        getHighScore();
-      } else {
-        var hint = 'Nope';
-        if (guess < x) hint += ' Too Small!';
-        if (guess > x) hint += ' Too Big!';
-        document.getElementById('hintText').innerHTML = hint;
-      }
-    }
-    console.log(count);
+		sendScore();
+		getHighScore();
+		$('#guess_btn').hide();
+		$('#again_btn').show();
+	} else {
+		var hint = 'Nope ' + guess +' is Too';
+		if (guess < x) hint += ' Small!';
+		if (guess > x) hint += ' Big!';
+		document.getElementById('hintText').innerHTML = hint;
+	}
+}
+
+function again(){
+	location.reload();
+}
+
+$( document ).ready(function(){
+getHighScore();
+$("#numberGuess").keypress(function(e) {
+	if(e.which == 13) {
+		checkGuess();
+	}
+});
+}
+);
+
+
