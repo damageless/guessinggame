6da5dd53d4633422d1db194b44cefdb6053260f6
Bones687 <neildalton6@gmail.com>
2015-01-14 21:10:10 -0700

    fixed

--- GetHighScore.php
+++ GetHighScore.php
@@ -1,7 +1,7 @@
 <?php
 	ini_set('display_errors', 1);
 	error_reporting(E_ALL);
-	$mysqli = new mysqli(‘localhost’, ‘kraczekn_game’, ‘testing1234’, ‘kraczekn_game’);
+	$mysqli = new mysqli('localhost', 'kraczekn_game', 'testing1234');
 
 	$query = “SELECT MIN(Try) FROM Table”;
 	if(!$result = $mysqli->query($query))
--- SaveScore.php
+++ SaveScore.php
@@ -1,15 +1,15 @@
 <?php
 	ini_set('display_errors', 1);
 	error_reporting(E_ALL);
-	$try = $_POST[‘turns’];
-	$mysqli = new mysqli(‘localhost’, ‘kraczekn_game’, ‘testing1234’, ‘kraczekn_game’);
+	$try = $_POST['turns'];
+	$mysqli = new mysqli('localhost', 'kraczekn_game', 'testing1234', 'kraczekn_game');
 
 	if (mysqli_connect_errno()) {
     		printf("Connect failed: %s\n", mysqli_connect_error());
     		exit();
 	}
 	$sql = ‘INSERT INTO Table (Try) VALUES (?)’;
-	$stmt->bind_param(“sss”, $try);
+	$stmt->bind_param("sss", $try);
 	$stmt = $mysqli->prepare($sql);
 
 	$stmt->execute();
